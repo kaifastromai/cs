3748f2f7672eb23cb67ad0f9775119c4ccbbc648
kaifas <cs.stormindustries@outlook.com>
2019-02-22 23:51:35 -0600

    fixes!

--- prj2_maps.cpp
+++ prj2_maps.cpp
@@ -8,29 +8,36 @@
 using namespace std;
 
 //parses a file by spliting on all punctuation/spaces/tabs/endlines and then sends to a txt doc line by line.
-string parse_file_to_txt(string filename)
+void parse_file_to_txt(string filename)
 {
 	ifstream inp;
 	ofstream onp;
 	onp.open("output.txt");
 	inp.open(filename);
-	if (inp.is_open())
+	if (inp.is_open() && onp.is_open())
 	{
+		cout << "Files are opened!" << endl;
 		for (string line; getline(inp, line);)
 		{
 			if (!line.empty())
 			{
+				cout << "Line is not empty!" << endl;
 				string word;
 				for (stringstream ss; ss >> word;)
 				{
+					cout << "The string is: " << ss.str() << endl;
 					stringstream s;
 
 					for (char c : word)
 					{
 						s << c;
 					}
-					onp << s.str();
+					onp << "Hello from the other side" << endl;
 				}
+			}
+			else
+			{
+				cout << "Line is empty!" << endl;
 			}
 		}
 		onp.close();
@@ -47,7 +54,7 @@ int main(int argc, char *argv[])
 {
 	if (argc > 1)
 	{
-		parse_file_to_txt(argv[2]);
+		parse_file_to_txt(argv[1]);
 		cout << "entered main" << endl;
 	}
 

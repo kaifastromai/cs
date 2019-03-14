13527943486902f0add1ea377ce217fe64b0c060
kaifas <cs.stormindustries@outlook.com>
2019-03-14 10:26:57 -0500

    project 3 started

---
+++ p3/prj3.cpp
@@ -0,0 +1,12 @@
+//
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <list>
+
+int main(int argc, char *argv[])
+{
+    std::cout.imbue(std::locale(""));
+    
+}
